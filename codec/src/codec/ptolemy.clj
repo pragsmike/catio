a68f92e02467c6e7a6454ea38dda95269c3f38cd
Mike Gallaher <152364+pragsmike@users.noreply.github.com>
2020-02-27 20:12:44 -0500

    Added ptolemy experiments.

---
+++ codec/src/codec/ptolemy.clj
@@ -0,0 +1,69 @@
+(ns codec.ptolemy
+  "If you get an error like \"Invalid DTD\" when you parse a MoML file,
+  you may have to change the DTD directive at the top of the MoML file
+  to have the correct URL -- it must be =https:= not =http:=.
+
+  property can have nested properties, in content.  This is used
+  to create groups of properties (eg, Annotations) or attach constraints
+  such as height and width to properties named 'text'.
+
+  property has class and name attributes (Java instance).
+  It can have a value attribute, or a content vector.
+
+  entity
+    name
+    class
+    property*
+  "
+  (:require [clojure.xml :as xml]
+            [clojure.string :as str]))
+
+(defn keywordize [s]
+  (keyword (str/replace s #"[ \t\n]" "_")))
+
+(defn construct-one [acc child]
+  (case (first child)
+    :property (let [[_ k v] child] (assoc-in acc [:properties k] v))
+    acc
+    ))
+
+(defn construct [children]
+  (reduce construct-one {} children))
+
+
+(defn valspec->val [attrs]                  ; :name :class :value
+  (case (:class attrs)
+   "ptolemy.data.expr.SingletonParameter" [(keywordize (:name attrs))
+                                            (read-string (:value attrs))]
+    "ptolemy.data.expr.Parameter" [(keywordize (:name attrs))
+                                   (read-string (:value attrs))]
+    "ptolemy.kernel.util.StringAttribute" [(keywordize (:name attrs))
+                                           (:value attrs)]
+
+    [(keywordize (:name attrs)) (:value attrs)]
+    )
+  )
+
+(defn pt-property [moml]
+  (valspec->val (:attrs moml))
+  )
+
+(defn pt-element [moml]
+  (let [attrs (:attrs moml)]
+    (case (:tag moml)
+      :entity [:entity :attrs attrs
+               (pt-blocks (:content moml))
+               ]
+
+      :property (pt-property moml)
+      moml
+      )))
+
+(defn pt-blocks [moml]
+  (cond (map? moml) (pt-element moml)
+        (seqable? moml)  (map pt-blocks moml)
+    ))
+
+
+(comment
+  (def pt (clojure.xml/parse "test/SoundSpectrum.xml")))
---
+++ codec/test/codec/ptolemy_test.clj
@@ -0,0 +1,117 @@
+(ns codec.ptolemy-test
+  (:require [clojure.test :refer :all]
+            [codec.ptolemy :refer :all]) )
+
+(defn sample-pt [name]
+  (clojure.xml/parse "test/SoundSpectrum.xml"))
+
+(def valspec-1 {:value "true"
+                :class "ptolemy.data.expr.SingletonParameter"
+                :name "_hideName"})
+(def valspec-2 {:class "ptolemy.actor.gui.style.TextStyle"
+                :name "_style"})
+(def valspec-3 {:value "20"
+                :class "ptolemy.data.expr.Parameter"
+                :name "height"})
+(def valspec-4 {:value "The name of the riddle"
+                :class "ptolemy.kernel.util.StringAttribute"
+                :name "text"})
+
+(deftest test-valspec->val
+  (is (= [:_hideName true] (valspec->val valspec-1)))
+  (is (= [:_style nil] (valspec->val valspec-2)))
+  (is (= [:height 20] (valspec->val valspec-3)))
+  (is (= [:text "The name of the riddle"] (valspec->val valspec-4)))
+  )
+
+(def prop-2 {:tag :property
+             :attrs valspec-1
+             :content nil})
+
+(deftest test-prop-2
+  (is (= [:_hideName true] (pt-property prop-2))))
+
+
+
+(def prop-3 {:tag :property
+             :attrs
+             {:value
+              "The model of computation is\nsynchronous dataflow (SDF)."
+              :class "ptolemy.kernel.util.StringAttribute"
+              :name "text"}
+             :content
+             [{:tag :property
+               :attrs
+               {:class "ptolemy.actor.gui.style.TextStyle" :name "_style"}
+               :content
+               [{:tag :property
+                 :attrs
+                 {:value "20"
+                  :class "ptolemy.data.expr.Parameter"
+                  :name "height"}
+                 :content nil}
+                {:tag :property
+                 :attrs
+                 {:value "80"
+                  :class "ptolemy.data.expr.Parameter"
+                  :name "width"}
+                 :content nil}]}]})
+
+(deftest test-prop-3
+  (is (= [:text "The model of computation is\nsynchronous dataflow (SDF)."
+          ]
+         (pt-property prop-3)))
+  )
+
+(def expected-3
+  {:name "property"
+   :content {:name "text"
+             :class "ptolemy.kernel.util.StringAttribute"
+             :value "The model of computation is\nsynchronous dataflow (SDF)."
+             :content [
+                       {:class "ptolemy.actor.gui.style.TextStyle"
+                        :name "_style"
+                        :content [{:value "20"
+                                   :class "ptolemy.data.expr.Parameter"
+                                   :name "height"}
+                                  {:value "80"
+                                   :class "ptolemy.data.expr.Parameter"
+                                   :name "width"}
+                                  ]}]}})
+
+(def expected-4
+  {:name "property"
+   :content {:name "text"
+           :class "ptolemy.kernel.util.StringAttribute"
+           :value "The model of computation is\nsynchronous dataflow (SDF)."
+           :content [
+                     {:class "ptolemy.actor.gui.style.TextStyle"
+                      :name "_style"
+                      :content [{:value "20"
+                                 :class "ptolemy.data.expr.Parameter"
+                                 :name "height"}
+                                {:value "80"
+                                 :class "ptolemy.data.expr.Parameter"
+                                 :name "width"}
+                                ]}]}})
+
+
+
+
+(deftest test-construct
+  (is (= {:properties {:a "1" :b "2"}} (construct [[:property :a "1"] [:property :b "2"]
+                                     [:something-else]])))
+  )
+
+(deftest test-ptolemy
+  (let [pt (sample-pt "SoundSpectrum")]
+    #_(is (= nil (pt-blocks pt)))
+    ))
+
+
+
+
+
+
+
+

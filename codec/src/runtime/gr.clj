1027321586de94da92593f90caa2e967945b9358
Mike Gallaher <152364+pragsmike@users.noreply.github.com>
2020-03-04 19:25:44 -0500

    GR 3.8 runtime runs from clojure via libpython-clj.

--- codec/src/runtime/gr.clj
+++ codec/src/runtime/gr.clj
@@ -1,4 +1,4 @@
-(ns rad.gr
+(ns runtime.gr
   "This might work, if gnuradio support Python >= 3.6,
   as libpython-clj requires Python 3.6 or later."
   (:require [libpython-clj.require :refer [require-python]]

c422a9b2585f6b99b1a59d28ecef2ea710871a3d
Mike Gallaher <152364+pragsmike@users.noreply.github.com>
2020-03-13 09:36:27 -0400

    Organized as stack project.

--- codec/src/lua/Lib.hs
+++ codec/src/lua/Lib.hs
@@ -4,9 +4,11 @@ module Lib
 import qualified Data.EDN as EDN
 import qualified Data.Text.IO as Text
 
+sample = "test/two.edn"
+
 main = do
-  edn <- Text.readFile "example.edn"
-  either error print $ EDN.parseText "example.edn" edn
+  edn <- Text.readFile sample
+  either error print $ EDN.parseText sample edn
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"

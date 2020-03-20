2eba835fe563f931fde62f9600900e3951c94302
pragsmike <152364+pragsmike@users.noreply.github.com>
2020-03-20 11:13:48 -0400

    Read Gnuradio GRC YAML

--- codec/src/lua/Lib.hs
+++ codec/src/lua/Lib.hs
@@ -1,14 +1,71 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 module Lib
-    ( someFunc
+    ( main3
     ) where
 import qualified Data.EDN as EDN
 import qualified Data.Text.IO as Text
+import qualified Data.Yaml as Y
+import qualified Data.ByteString.Char8 as BS
+import GHC.Generics
+import Data.Map
+
+------------------------------------------------------------------------
+-- Given block parameters, construct a program fragment
+
+type NodeFragment = [Char]
+type EdgeFragment = [Char]
+
+data Fragment = NodeFragment | EdgeFragment
+
+
+  -- what gets read from grc file
+data Node = Node [String]
+data Edge = Edge Node Node
+data Graph = Graph [Node] [Edge]
+type Spec = String
+
+
+top :: Spec -> String
+top s
+  | s == "" = ""
+  | otherwise = ""
+
+preamble :: IO ()
+preamble = putStrLn "local radio = require('radio')\n"
+
+body :: IO ()
+body = putStrLn (top "")
+
+postamble :: IO ()
+postamble = putStrLn "top:run()"
+
+main2 :: IO ()
+main2 = do
+  preamble
+  body
+  postamble
+
+------------------------------------------------------------------------
+
+-- data GRC = GRC { options :: Map
+--                      , blocks :: Map
+--                      , connections :: Map
+--                      } deriving (Show, Generic)
+-- instance Y.FromJSON GRC
+
+
+sampleYaml = "test/two-yaml.grc"
+main3 :: IO ()
+main3 = do
+  yamls <- BS.readFile sampleYaml
+  res <- Y.decodeThrow yamls :: IO Y.Value
+  putStrLn (show res)
+
+------------------------------------------------------------------------
 
 sample = "test/two.edn"
-
+main :: IO ()
 main = do
   edn <- Text.readFile sample
   either error print $ EDN.parseText sample edn
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"

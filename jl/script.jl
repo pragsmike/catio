b847c0961590ab5b5ac2d2059c97fd901bd1669c
Mike Gallaher <152364+pragsmike@users.noreply.github.com>
2020-07-27 07:52:28 -0400

    Channel recognizer.

--- jl/script.jl
+++ jl/script.jl
@@ -7,7 +7,6 @@ freq = 454.3501e6
 samprate = 1200000
 
 samps = Array{ComplexF32}(undef, num_bins)
-avg = zeros(ComplexF32, num_bins)
 
 function fillsamps(f, dest)
     for n in eachindex(dest)
@@ -31,33 +30,50 @@ function thresh(bins, th)
     end
 end
 
-history_size = 100
-history = zeros(ComplexF32, num_bins, history_size)
+history_size = 10
+history = zeros(Float32, num_bins, history_size)
 history_i = 0
 
-function mavg(bins)
+function mavg(avg, bins)
     for i in eachindex(bins)
         global history_i += 1
-        history[i, history_i] = bins[i]
+        global history[i, history_i] = bins[i]
         if history_i >= history_size; history_i = 0; end
         t = sum(history[i,:])/history_size
-        avg[i] = t
+        global avg[i] = t
+    end
+    return avg
+end
+
+function plot_hists(bins)
+    bins = fftshift(bins)
+    PyPlot.plot(bins)
+end
+
+
+function add_to_hists(bins, hist_5)
+    for i in eachindex(bins)
+        if (bins[i] > .09)
+            hist_5[i] += 1
+        end
     end
 end
 
 function dofile(fn)
     open(fn) do f
+        hist_5 = zeros(Int, num_bins)
+        avg = zeros(Float32, num_bins)
         for i in 1:50000
             fillsamps(f,samps)
             bins = fft(samps)
-            mavg(bins)
+            magbins = abs.(bins)
+            avgmagbins = mavg(avg, magbins)
+            add_to_hists(avgmagbins, hist_5)
             if i % 25000 == 0
-                tavg = fftshift(avg)
-                db = 20 .*log10.(abs.(tavg))
-                thresh(db, -30)
-                PyPlot.plot(db)
+                println(avgmagbins[1])
             end
         end
+        plot_hists(hist_5)
     end
 end
 

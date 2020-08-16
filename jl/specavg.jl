9d7fe923ba82b00768f82d7c260154afddf1aa51
Mike Gallaher <152364+pragsmike@users.noreply.github.com>
2020-08-16 13:54:30 -0400

    Refactor specavg.

--- jl/specavg.jl
+++ jl/specavg.jl
@@ -1,4 +1,4 @@
-using FFTW, PyPlot, DSP
+using FFTW, PyPlot, DSP,Test,Printf
 
 infn = "/home/mg/Documents/signals/fft_frames"
 outfn = "/tmp/fft_frames_avg"
@@ -7,21 +7,33 @@ const num_bins = 2048
 
 const history_size = 100
 
-function mavg!(avg, bins, history, history_i)
-    for i in eachindex(bins)
-        history[i, history_i] = bins[i]
-        avg[i] = sum(history[i,:])/history_size
-    end
-    history_i += 1
-    if history_i >= history_size; history_i = 1; end
-    return history_i
+mutable struct MA
+    avgmagbins::Array{Float32}
+    history::Array{Float32, 2}
+    history_i::Int32
 end
+
+function newMA(num_bins, history_size)
+    return MA(zeros(Float32, num_bins),
+              zeros(Float32, num_bins, history_size),
+              1)
+end
+
+function add!(ma::MA, magbins)
+    history_size = size(ma.history)[2]
+    for i in eachindex(magbins)
+        ma.history[i, ma.history_i] = magbins[i]
+        ma.avgmagbins[i] = sum(ma.history[i,:])/history_size
+    end
+    ma.history_i += 1
+    if ma.history_i > history_size; ma.history_i = 1; end
+end
+
+
 
 function dofile(infn, outfn)
     magbins = Array{Float32}(undef, num_bins)
-    avgmagbins = zeros(Float32, num_bins)
-    history = zeros(Float32, num_bins, history_size)
-    history_i = 1
+    ma = newMA(num_bins, history_size)
     open(infn) do inf
         open(outfn, "w") do outf
             for i in 1:6000000
@@ -31,14 +43,35 @@ function dofile(infn, outfn)
                     @printf("Input ended after reading %i frames\n", i)
                     break
                 end
-                history_i = mavg!(avgmagbins, magbins, history, history_i)
-                write(outf, avgmagbins)
+                add!(ma, magbins)
+                write(outf, ma.avgmagbins)
                 if i % 25000 == 0
-                    println(avgmagbins[18])
+                    println(ma.avgmagbins[18])
                 end
             end
         end
     end
 end
 
-@time dofile(infn, outfn)
+@testset "moving average sanity checks" begin
+    magbins = ones(Float32, num_bins)
+
+    ma = newMA(num_bins, history_size)
+    add!(ma, magbins)
+
+    @test ma.avgmagbins ≈ fill(0.01, num_bins)
+
+    add!(ma, magbins)
+
+    @test ma.avgmagbins ≈ fill(0.02, num_bins)
+
+    for i in 1:200
+        add!(ma, magbins)
+    end
+    @test ma.history[1,:] ≈ fill(1.0, history_size)
+
+    @test ma.avgmagbins ≈ fill(1.0, num_bins)
+
+end
+
+#@time dofile(infn, outfn)
